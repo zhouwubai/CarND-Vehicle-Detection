ce278e7f003bba22bee9f5dbe0d99fe4586521ce
Wubai Zhou <wubai.zhou@uber.com>
2018-02-13 14:21:02 -0800

    fix bugs for line detection

--- src/line/models.py
+++ src/line/models.py
@@ -83,8 +83,8 @@ class LineDetector(object):
 
     def process_image(self, image):
         # step one: undistorted image
-        import pdb
-        pdb.set_trace()
+        # import pdb
+        # pdb.set_trace()
         thresh_names, n_vote = ['S', 'R', 'X'], 2
         shape = image.shape
         img_size = (shape[1], shape[0])
@@ -118,48 +118,14 @@ class LineDetector(object):
             is_passed, left_line, right_line =\
                 self.sanityCheck(left_line, right_line, True)
 
-        left_fit, right_fit = left_line.coeffs, right_line.coeffs
-        ploty = np.linspace(0, shape[0] - 1, shape[0])
-        left_fitx =\
-            left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-        right_fitx =\
-            right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
-
-        unwarp_zero = np.zeros_like(binary_unwarped).astype(np.uint8)
-        color_unwarp = np.dstack((unwarp_zero, unwarp_zero, unwarp_zero))
-
-        # Recast the x and y points into usable format for cv2.fillPoly()
-        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx,
-                                                                ploty])))])
-        pts = np.hstack((pts_left, pts_right))
-
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(color_unwarp, np.int_([pts]), (0, 255, 0))
-        newwarp = cv2.warpPerspective(color_unwarp,
-                                      self.unwarper.inv_M, img_size)
-
-        # get the curvature and put text
-        left_curve = left_line.radius_of_curvature
-        right_curve = right_line.radius_of_curvature
-
-        avg_curve = (left_curve + right_curve) / 2
-        curvature_txt = "Radius of Curvature = {:.0f}(m)".format(avg_curve)
-        deviation = Line.deviate_of_center(left_line, right_line)
-        if deviation >= 0:
-            dtxt = "Vechicle is {:.2f}(m) right of center".format(deviation)
-        else:
-            dtxt = "Vechicle is {:.2f}(m) left of center".format(-deviation)
-
-        cv2.putText(undistorted, curvature_txt, (100, 100),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1.5, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(undistorted, dtxt, (100, 160),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1.5, (255, 255, 255), 2, cv2.LINE_AA)
-
-        # Combine the result with the original image
-        result = cv2.addWeighted(undistorted, 1, newwarp, 0.3, 0)
+        result = self.merge_drawing(undistorted=undistorted,
+                                    binary_warped=binary_warped,
+                                    binary_unwarped=binary_unwarped,
+                                    left_line=left_line,
+                                    right_line=right_line,
+                                    left_windows=left_windows,
+                                    right_windows=right_windows,
+                                    scale=4)
         return result
 
     def merge_drawing(self, undistorted, binary_warped, binary_unwarped,
@@ -184,12 +150,16 @@ class LineDetector(object):
         right_fitx =\
             right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
 
+        ploty = ploty.astype(np.int32)
+        left_fitx, right_fitx =\
+            left_fitx.astype(np.int32), right_fitx.astype(np.int32)
+
         # draw line and windows on this
         unwarped_line_win =\
             np.dstack((binary_unwarped, binary_unwarped, binary_unwarped))
         unwarped_line_win = 255 * unwarped_line_win.astype(np.uint8)
-        unwarped_line_win[ploty, left_fitx.astype(np.uint8)] = [255, 0, 0]
-        unwarped_line_win[ploty, right_fitx.astype(np.uint8)] = [0, 0, 255]
+        unwarped_line_win[ploty, left_fitx] = [255, 0, 0]
+        unwarped_line_win[ploty, right_fitx] = [0, 0, 255]
         unwarped_line_win =\
             draw_boxes(unwarped_line_win, left_windows,
                        color=(255, 0, 0), thick=5, inplace=True)
@@ -205,7 +175,7 @@ class LineDetector(object):
                                     cv2.BORDER_CONSTANT, value=(255, 0, 0))
         panel2 = cv2.copyMakeBorder(unwarped_line_win, 10, 10, 5, 10,
                                     cv2.BORDER_CONSTANT, value=(255, 0, 0))
-        info_panel = np.vstack((panel1, panel2))
+        info_panel = np.hstack((panel1, panel2))
         info_panel_small = cv2.resize(info_panel,
                                       (info_panel.shape[1] // scale,
                                        info_panel.shape[0] // scale))
@@ -239,10 +209,10 @@ class LineDetector(object):
         else:
             dtxt = "Vechicle is {:.2f}(m) left of center".format(-deviation)
 
-        cv2.putText(undistorted, curvature_txt, (100, 100),
+        cv2.putText(undistorted, curvature_txt, (20, 40),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(undistorted, dtxt, (100, 160),
+        cv2.putText(undistorted, dtxt, (20, 100),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1, (255, 255, 255), 2, cv2.LINE_AA)
 
